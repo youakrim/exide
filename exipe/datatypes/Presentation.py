2c58bd5ff2e44724f5dbdca892016f77e474d535
Codophile1 <yanisouakrim@yahoo.fr>
2017-05-30 18:27:48 +0200

    [UPD] Code cleaning

--- exipe/datatypes/Presentation.py
+++ exipe/datatypes/Presentation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
 
 class Presentation:
-	def __init__(self, section):
-		self.root_section = section
-		self.metadata = None
+    def __init__(self, section):
+        self.root_section = section
+        self.metadata = None
 
-	def get_slide_by_id(self, id):
-		pass
+    def get_slide_by_id(self, id):
+        pass
 
-	def export_to_json(self, ):
-		pass
-
+    def export_to_json(self, ):
+        pass
