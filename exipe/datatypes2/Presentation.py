095de76cb84735b56608db415b21cc0e85f62487
Codophile1 <yanisouakrim@yahoo.fr>
2017-05-30 10:49:31 +0200

    [UPD] datatype update

---
+++ exipe/datatypes2/Presentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Presentation:
+	def __init__(self):
+		self.root_section = None
+		self.metadata = None
+
+	def get_slide_by_id(self, id):
+		pass
+
+	def export_to_json(self, ):
+		pass
+
---
+++ exipe/datatypes2/Section.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Section:
+	def __init__(self):
+		self.title = None
+		self.subelements = None
+		self.level = None
+		self.toc_slide_id = None
+
+	def emphasized_text(self, ):
+		pass
+
+	def named_entities(self, ):
+		pass
+
+	def text(self, ):
+		pass
+
+	def urls(self, ):
+		pass
+
+	def slides(self, ):
+		pass
+
+	def outline(self, ):
+		pass
+
+	def get_slides_of_type(self, type):
+		pass
+
+	def get_slides_by_keyword(self, keyword):
+		pass
+
+	def get_slides_by_title(self, title):
+		pass
+
