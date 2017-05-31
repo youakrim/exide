055eb2a0404756ccaa727dd366419a510ef7dcf8
Codophile1 <yanisouakrim@yahoo.fr>
2017-05-31 15:53:33 +0200

    [UPD] Command line support

--- exipe/exipe.py
+++ exipe/exipe.py
@@ -1,9 +1,61 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+import getopt
+import os
+
+import sys
+
+import odp_parser
 
 
-def main():
-    pass
+# On cherche à récuperer les noms des fichiers d'entrée et de sortie
+import pptx_parser
 
-if __name__ == '__main__':
-    main()
+
+def main(argv):
+    # We try to get the input and output files names
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+        print("Opts : ")
+        print(opts)
+        print("Args : ")
+        print(args)
+    except getopt.GetoptError:
+        print('test.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    print('Input file is "', inputfile)
+    print('Output file is "', outputfile)
+
+    # On récupère les noms de fichiers et extensions
+    # We extract the filename and extension of both files
+    inputfile_name, inputfile_extension = os.path.splitext(inputfile)
+    inputfile_directory = os.path.dirname(inputfile)
+    print("L'extension du fichier d'entrée est", inputfile_extension)
+    # On vérifie que l'extension du fichier d'entrée (inputfile) est connue
+    # We check if the inputfile extension is recognized
+    extensions_connues = [".odp"]
+
+    if inputfile_extension not in extensions_connues:
+        sys.exit("Erreur : Type de fichier non supporté \n \t Les extensions supportées sont ", extensions_connues)
+
+    # Si odp alors on entre dans l'archive
+    # If the input file extension is ".odp", we unzip the archive
+    if inputfile_extension == ".odp":
+        pres = odp_parser.parse_odp(inputfile)
+        print pres.root_section.outline
+    elif inputfile_extension == ".pptx":
+        pres = pptx_parser.parse_pptx(inputfile)
+        print pres.root_section.outline
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
